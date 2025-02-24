e0e76bc52fffc7668f0e5a466df59a0d966369e6
Muuo123-dot <muuoj9996@gmail.com>
2025-02-24 23:35:17 +0300

    Rectangular area.C

--- Rectangular area.C
+++ Rectangular area.C
@@ -1,4 +1,4 @@
-//program to find the area of rectangle.
+//PROGRAM TO FIND THE AREA OF RECTANGLE.
 // the user is required to input the length and width of the rectangle.
 
 #include<stdio.h>
@@ -6,11 +6,10 @@ int main() {
   int length=0;
   int width=0;
   printf("length:\n");
-  scanf("%d",&length);//input length
+  scanf("%d",&length);
   printf("width:\n");
-  scanf("%d",&width);//input width
+  scanf("%d",&width);
   int area= length *width;
-  printf("area:%d\n",area);// output the area based on the formula A=L*W 
+  printf("area:%d\n",area); 
   return 0;
 }
-//the user can add different values to get different values of A.
